016ffa8a3c9aebca198e3a30e3e08e521676d773
ihsan-buneri <68999003+ihsan-buneri@users.noreply.github.com>
2025-07-21 10:27:51 +0500

    Initial React Setup with ShadeCN/UI

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+	return (
+		<div className='flex min-h-svh flex-col items-center justify-center'>
+			<h1 className='text-2xl font-bold mb-4'>Welcome to Cometra AI</h1>
+			<Button>Click me</Button>
+		</div>
+	);
+}
+
+export default App;
